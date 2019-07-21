7ac6ae6cba1032292f6e2f28db67a0c0e351005f
Priyanka Rohokale <Priyanka.Rohokale@nielsen.com>
2019-07-21 17:06:48 +0530

    few changes

--- src/app/product/container/product-shell/product-shell.component.ts
+++ src/app/product/container/product-shell/product-shell.component.ts
@@ -1,24 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Product } from 'src/app/shared/model/product.model';
 import { Observable } from 'rxjs';
 import { Store, select } from '@ngrx/store';
 import * as fromProduct from '../../store';
 import * as fromProductActions from '../../store/product.actions';
+import { takeWhile } from 'rxjs/operators';
 
 @Component({
   selector: 'app-product-shell',
   templateUrl: './product-shell.component.html',
   styleUrls: ['./product-shell.component.css']
 })
-export class ProductShellComponent implements OnInit {
+export class ProductShellComponent implements OnInit, OnDestroy {
   isSelected: boolean;
   products$: Observable<Product[]>;
+  componentActive: boolean = true;
   products: Product[];
   constructor(private store: Store<fromProduct.State>) { }
 
   ngOnInit() {
     this.products$ = this.store.pipe(select(fromProduct.getProducts));
-    this.store.pipe(select(fromProduct.getIsSelected))
+    this.store.pipe(select(fromProduct.getIsSelected), takeWhile(() => this.componentActive))
       .subscribe(res => {
         console.log(res);
         this.isSelected = res;
@@ -31,5 +33,9 @@ export class ProductShellComponent implements OnInit {
   productOutput(updatedproduct) {
     console.log(updatedproduct);
     this.store.dispatch(new fromProductActions.UpdateProduct(updatedproduct))
+  }
+
+  ngOnDestroy() {
+    this.componentActive = false
   }
 }
